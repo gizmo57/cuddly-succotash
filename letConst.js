a7e534ea994a61b7cbe07fdcc99f137cdd41ced5
gizmo57 <cgsisgriffin@gmail.com>
2023-03-24 12:49:08 -0400

    add comment

--- letConst.js
+++ letConst.js
@@ -1,6 +1,6 @@
-const PI =3.14
+// const PI =3.14
 
-var can be mutated outside of its block let is held to the block assignment
-var can be manipulated and const remains the same always it was not be changed
-?
-declaring variables before they are used in a function.
+// var can be mutated outside of its block let is held to the block assignment
+// var can be manipulated and const remains the same always it was not be changed
+// ?
+// declaring variables before they are used in a function
