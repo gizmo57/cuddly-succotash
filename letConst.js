04dda26896128baac83473a466fb0d0dcb792c0c
gizmo57 <cgsisgriffin@gmail.com>
2023-03-25 15:29:12 -0400

    arrow function

--- letConst.js
+++ letConst.js
@@ -4,3 +4,16 @@
 // var can be manipulated and const remains the same always it was not be changed
 // ?
 // declaring variables before they are used in a function
+
+// const arr = [1, 2, 3, 4];
+// let total = arr.reduce(function (accum, nxtnum) {
+//     console.log(accum, nxtnum)
+//     return accum + nxtnum
+// }, 0)
+
+// let multiple = arr.reduce(function(accum, nxtnum){
+//     return accum * nxtnum;
+// })
+const double = arr => arr.map(val => val * 2);
+
+const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0)
