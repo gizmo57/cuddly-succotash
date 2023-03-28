25b8fe1fca21c666b718cc6f5ab41117e70d5ead
gizmo57 <cgsisgriffin@gmail.com>
2023-03-28 13:14:47 -0400

    obj enhancement

--- letConst.js
+++ letConst.js
@@ -18,62 +18,96 @@
 
 // const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0)
 
-function filterOutOdds() {
-    var nums = Array.prototype.slice.call(arguments);
-    return nums.filter(function (num) {
-        return num % 2 === 0
-    });
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function (num) {
+//         return num % 2 === 0
+//     });
+// }
+// function filterOutOdds() {
+//     const nums = (...arguments)
+//   return nums.filter(function (num) {
+//         return num % 2 === 0
+//     });
+// }
+// const findMin = (...args) => Math.min(...args)
+
+// const merge = (obj1, obj2) => ({ ...obj1, ...obj2 })
+
+// const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v * 2)]
+
+
+
+// const removeRandom = items => {
+//     let idx = Math.floor(Math.random() * items.length);
+//     return [...items.slice(0, idx), ...items.slice(idx + 1)];
+// }
+
+
+
+// const extend = (array1, array2) => {
+//     return [...array1, ...array2];
+// }
+
+
+
+// const addKeyVal = (obj, key, val) => {
+
+//     let newObj = { ...obj }
+//     newObj[key] = val;
+//     return newObj;
+// }
+
+// const removeKey = (obj, key) => {
+
+//     // OPTION 1
+//     let newObj = { ...obj }
+//     delete newObj[key]
+//     return newObj;
+// }
+
+
+// const combine = (obj1, obj2) => {
+//     return { ...obj1, ...obj2 };
+// }
+
+
+// const update = (obj, key, val) => {
+//     let newObj = { ...obj }
+//     newObj[key] = val;
+//     return newObj;
+//
+
+function createInstructor(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
 }
-function filterOutOdds() {
-    const nums = (...arguments)
-  return nums.filter(function (num) {
-        return num % 2 === 0
-    });
-}
-const findMin = (...args) => Math.min(...args)
 
-const merge = (obj1, obj2) => ({ ...obj1, ...obj2 })
+let favoriteNumber = 42;
 
-const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v * 2)]
-
-
-
-const removeRandom = items => {
-    let idx = Math.floor(Math.random() * items.length);
-    return [...items.slice(0, idx), ...items.slice(idx + 1)];
+const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
 }
 
 
-
-const extend = (array1, array2) => {
-    return [...array1, ...array2];
+const instructor = {
+    firstName: "Colt",
+    sayHi() {
+        return "Hi!";
+    },
+    sayBye() {
+        return this.firstName + " says bye!";
+    }
 }
 
-
-
-const addKeyVal = (obj, key, val) => {
-
-    let newObj = { ...obj }
-    newObj[key] = val;
-    return newObj;
-}
-
-const removeKey = (obj, key) => {
-
-    // OPTION 1
-    let newObj = { ...obj }
-    delete newObj[key]
-    return newObj;
-}
-
-
-const combine = (obj1, obj2) => {
-    return { ...obj1, ...obj2 };
-}
-
-
-const update = (obj, key, val) => {
-    let newObj = { ...obj }
-    newObj[key] = val;
-    return newObj;
+function makeAnimal(species, verb, noise) {
+    return {
+        species,
+        [verb]() {
+            return noise;
+        }
+    }
 }
