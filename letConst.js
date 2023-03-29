3f72925a70df5d3091f985cb332fef6a38b5f993
gizmo57 <cgsisgriffin@gmail.com>
2023-03-28 21:58:18 -0400

    destructuring

--- letConst.js
+++ letConst.js
@@ -78,36 +78,71 @@
 //     return newObj;
 //
 
-function createInstructor(firstName, lastName) {
-    return {
-        firstName,
-        lastName
+// function createInstructor(firstName, lastName) {
+//     return {
+//         firstName,
+//         lastName
+//     }
+// }
+
+// let favoriteNumber = 42;
+
+// const instructor = {
+//     firstName: "Colt",
+//     [favoriteNumber]: "That is my favorite!"
+// }
+
+
+// const instructor = {
+//     firstName: "Colt",
+//     sayHi() {
+//         return "Hi!";
+//     },
+//     sayBye() {
+//         return this.firstName + " says bye!";
+//     }
+// }
+
+// function makeAnimal(species, verb, noise) {
+//     return {
+//         species,
+//         [verb]() {
+//             return noise;
+//         }
+//     }
+// }
+
+8
+1846
+
+yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659
+
+Your name is Alejandro and you like purple
+Your name is Melissa and you like green
+Your name is undefined and you like green
+
+Maya
+Marisa
+Chi
+
+"raindrops on roses"
+"whiskers on kittens"
+["Bright copper kettles",
+"warm woolen mittens",
+    "Brown paper packages tied up with strings"];
+
+10, 30, 20
+
+const obj = {
+    numbers: {
+        a: 1,
+        b: 2
     }
-}
+};
 
-let favoriteNumber = 42;
+const { a, b } = obj.numbers
 
-const instructor = {
-    firstName: "Colt",
-    [favoriteNumber]: "That is my favorite!"
-}
+[arr[0], arr[1]] = [arr[1], arr[0]]
 
-
-const instructor = {
-    firstName: "Colt",
-    sayHi() {
-        return "Hi!";
-    },
-    sayBye() {
-        return this.firstName + " says bye!";
-    }
-}
-
-function makeAnimal(species, verb, noise) {
-    return {
-        species,
-        [verb]() {
-            return noise;
-        }
-    }
-}
+raceResults(["Corn", "Mary", "Kay", "Marcie"])
+const raceResults([first, second, third, ...rest]) => ({ first, second, third, rest })
